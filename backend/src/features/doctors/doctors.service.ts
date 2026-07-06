6da9a0afce96ffb077ee8f4bf76f26b3941c754d
Austin Nathaniel Miranda <donovan.austin.ad@gmail.com>
2026-07-06 13:34:24 +0800

    chore(be): added comments for doctor service for better readability

--- backend/src/features/doctors/doctors.service.ts
+++ backend/src/features/doctors/doctors.service.ts
@@ -1,6 +1,8 @@
 import { pool } from "../../db";
 
 export async function listDoctors() {
+
+  // Select all doctors with the doctor details sorted by last name and first name
   const result = await pool.query(
     `SELECT id, title, first_name, middle_name, last_name, specialty
      FROM users
